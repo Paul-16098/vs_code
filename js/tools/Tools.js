e50d2a6266a73f6b418673a7f7cf49a61bfbe2fb
Paul-16098 <pl816098@gmail.com>
2024-02-22 18:43:32 +0800

    modified:   js/tools/Tools.js

--- js/tools/Tools.js
+++ js/tools/Tools.js
@@ -13,125 +13,128 @@
 
 const _unsafeWindow =
   typeof unsafeWindow === "undefined" ? window : unsafeWindow; //兼容 ios userscripts 的寫法
-set_gm: {
-  var _GM_xmlhttpRequest,
-    _GM_registerMenuCommand,
-    _GM_notification,
-    _GM_addStyle,
-    _GM_openInTab,
-    _GM_info,
-    _GM_setClipboard;
-  GM_xmlhttpRequest: {
-    if (typeof GM_xmlhttpRequest !== "undefined") {
-      _GM_xmlhttpRequest = GM_xmlhttpRequest;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.xmlHttpRequest !== "undefined"
-    ) {
-      _GM_xmlhttpRequest = GM.xmlHttpRequest;
-    } else {
-      _GM_xmlhttpRequest = (f) => {
-        fetch(f.url, {
-          method: f.method || "GET",
-          body: f.data,
-          headers: f.headers,
-        })
-          .then((response) => response.text())
-          .then((data) => {
-            f.onload && f.onload({ response: data });
+function set_gm() {
+  set_gm: {
+    var _GM_xmlhttpRequest,
+      _GM_registerMenuCommand,
+      _GM_notification,
+      _GM_addStyle,
+      _GM_openInTab,
+      _GM_info,
+      _GM_setClipboard;
+    GM_xmlhttpRequest: {
+      if (typeof GM_xmlhttpRequest !== "undefined") {
+        _GM_xmlhttpRequest = GM_xmlhttpRequest;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.xmlHttpRequest !== "undefined"
+      ) {
+        _GM_xmlhttpRequest = GM.xmlHttpRequest;
+      } else {
+        _GM_xmlhttpRequest = (f) => {
+          fetch(f.url, {
+            method: f.method || "GET",
+            body: f.data,
+            headers: f.headers,
           })
-          .catch(f.onerror && f.onerror());
-      };
+            .then((response) => response.text())
+            .then((data) => {
+              f.onload && f.onload({ response: data });
+            })
+            .catch(f.onerror && f.onerror());
+        };
+      }
     }
-  }
-  GM_registerMenuCommand: {
-    if (typeof GM_registerMenuCommand !== "undefined") {
-      _GM_registerMenuCommand = GM_registerMenuCommand;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.registerMenuCommand !== "undefined"
-    ) {
-      _GM_registerMenuCommand = GM.registerMenuCommand;
-    } else {
-      _GM_registerMenuCommand = (s, f) => {
-        debug(s);
-        debug(f);
-      };
+    GM_registerMenuCommand: {
+      if (typeof GM_registerMenuCommand !== "undefined") {
+        _GM_registerMenuCommand = GM_registerMenuCommand;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.registerMenuCommand !== "undefined"
+      ) {
+        _GM_registerMenuCommand = GM.registerMenuCommand;
+      } else {
+        _GM_registerMenuCommand = (s, f) => {
+          debug(s);
+          debug(f);
+        };
+      }
     }
-  }
-  GM_info: {
-    if (typeof GM_info !== "undefined") {
-      _GM_info = GM_info;
-    } else if (typeof GM !== "undefined" && typeof GM.info !== "undefined") {
-      _GM_info = GM.info;
-    } else {
-      _GM_info = { script: {} };
+    GM_info: {
+      if (typeof GM_info !== "undefined") {
+        _GM_info = GM_info;
+      } else if (typeof GM !== "undefined" && typeof GM.info !== "undefined") {
+        _GM_info = GM.info;
+      } else {
+        _GM_info = { script: {} };
+      }
     }
-  }
-  GM_notification: {
-    if (typeof GM_notification !== "undefined") {
-      _GM_notification = GM_notification;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.notification !== "undefined"
-    ) {
-      _GM_notification = GM.notification;
-    } else {
-      _GM_notification = (s) => {
-        showTips(String(s.text || s));
-      };
+    GM_notification: {
+      if (typeof GM_notification !== "undefined") {
+        _GM_notification = GM_notification;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.notification !== "undefined"
+      ) {
+        _GM_notification = GM.notification;
+      } else {
+        _GM_notification = (s) => {
+          showTips(String(s.text || s));
+        };
+      }
     }
-  }
-  GM_openInTab: {
-    if (typeof GM_openInTab !== "undefined") {
-      _GM_openInTab = GM_openInTab;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.openInTab !== "undefined"
-    ) {
-      _GM_openInTab = GM.openInTab;
-    } else {
-      _GM_openInTab = (s, t) => {
-        window.open(s);
-        debug(t);
-      };
+    GM_openInTab: {
+      if (typeof GM_openInTab !== "undefined") {
+        _GM_openInTab = GM_openInTab;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.openInTab !== "undefined"
+      ) {
+        _GM_openInTab = GM.openInTab;
+      } else {
+        _GM_openInTab = (s, t) => {
+          window.open(s);
+          debug(t);
+        };
+      }
     }
-  }
-  GM_addStyle: {
-    if (typeof GM_addStyle !== "undefined") {
-      _GM_addStyle = GM_addStyle;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.addStyle !== "undefined"
-    ) {
-      _GM_addStyle = GM.addStyle;
-    } else {
-      _GM_addStyle = (cssStr = null) => {
-        let styleEle = document.createElement("style");
-        styleEle.classList.add("_GM_addStyle");
-        styleEle.innerHTML = cssStr;
-        document.head.appendChild(styleEle);
-        return styleEle;
-      };
+    GM_addStyle: {
+      if (typeof GM_addStyle !== "undefined") {
+        _GM_addStyle = GM_addStyle;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.addStyle !== "undefined"
+      ) {
+        _GM_addStyle = GM.addStyle;
+      } else {
+        _GM_addStyle = (cssStr = null) => {
+          let styleEle = document.createElement("style");
+          styleEle.classList.add("_GM_addStyle");
+          styleEle.innerHTML = cssStr;
+          document.head.appendChild(styleEle);
+          return styleEle;
+        };
+      }
     }
-  }
-  GM_setClipboard: {
-    if (typeof GM_setClipboard !== "undefined") {
-      _GM_setClipboard = GM_setClipboard;
-    } else if (
-      typeof GM !== "undefined" &&
-      typeof GM.setClipboard !== "undefined"
-    ) {
-      _GM_setClipboard = GM.setClipboard;
-    } else {
-      _GM_setClipboard = (s, i) => {
-        debug(s);
-        debug(i);
-      };
+    GM_setClipboard: {
+      if (typeof GM_setClipboard !== "undefined") {
+        _GM_setClipboard = GM_setClipboard;
+      } else if (
+        typeof GM !== "undefined" &&
+        typeof GM.setClipboard !== "undefined"
+      ) {
+        _GM_setClipboard = GM.setClipboard;
+      } else {
+        _GM_setClipboard = (s, i) => {
+          debug(s);
+          debug(i);
+        };
+      }
     }
   }
 }
-_unsafeWindow.console.debug = function (str, title = "INFO", type = "log") {
+
+_unsafeWindow.Tools.debug = function (str, title = "INFO", type = "log") {
   if (
     !(type === "log" || type === "info" || type === "error" || type === "warn")
   ) {
@@ -1571,8 +1574,8 @@ class Tools {
   };
 }
 _unsafeWindow.Tools = Tools;
-console.debug(
-  `if (Tools.getUrlParam('on') === 'true' || Tools.getUrlParam('on') === null) = ${
-    Tools.getUrlParam("on") === "true" || Tools.getUrlParam("on") === null
-  }`
+_unsafeWindow.set_gm = set_gm;
+console.log(
+  `if (Tools.getUrlParam('on') === 'true' || Tools.getUrlParam('on') === null) = `,
+  Tools.getUrlParam("on") === "true" || Tools.getUrlParam("on") === null
 );
