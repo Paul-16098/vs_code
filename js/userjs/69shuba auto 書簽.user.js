568e4879d7c5da6e5c03a1835bf8b339d2087747
Paul-16098 <pl816098@gmail.com>
2024-02-22 12:51:00 +0800

    modified:   "js/userjs/69shuba auto \346\233\270\347\260\275.user.js"

--- js/userjs/69shuba auto 書簽.user.js
+++ js/userjs/69shuba auto 書簽.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         69shuba auto 書簽
 // @namespace    pl816098
-// @version      2.7.2
+// @version      2.7.3
 // @description  自動書籤,更改css,可以在看書頁(https://www.69shuba.com/txt/*/*)找到作者連結
 // @author       pl816098
 // @match        https://www.69shuba.com/txt/*/*
