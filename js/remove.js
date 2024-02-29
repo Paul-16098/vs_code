5e9f91d689f38bb44addd6fe2f7fd1b2783bc32f
Paul-16098 <pl816098@gmail.com>
2024-02-29 17:04:31 +0800

    modified:   js/remove.js

--- js/remove.js
+++ js/remove.js
@@ -3,20 +3,26 @@
 function remove(str, ...args) {
   if (typeof str === "string") {
     if (document.querySelector(str)) {
-      document.querySelector(str).remove();
+      document.querySelectorAll(str).forEach((ele) => {
+        ele.remove();
+      });
+    } else if (typeof str === "object" && str.length > 0) {
+      str.forEach((str) => {
+        if (document.querySelector(str)) {
+          document.querySelectorAll(str).forEach((ele) => {
+            ele.remove();
+          });
+        }
+      });
     }
-  } else if (typeof str === "object" && str.length > 0) {
-    str.forEach((str) => {
-      if (document.querySelector(str)) {
-        document.querySelector(str).remove();
-      }
-    });
-  }
-  if (args && args.length > 0) {
-    args.forEach((args) => {
-      if (document.querySelector(args)) {
-        document.querySelector(args).remove();
-      }
-    });
+    if (args && args.length > 0) {
+      args.forEach((args) => {
+        if (document.querySelector(args)) {
+          document.querySelectorAll(args).forEach((ele) => {
+            ele.remove();
+          });
+        }
+      });
+    }
   }
 }
