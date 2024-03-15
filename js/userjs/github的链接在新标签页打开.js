f0f1ff63a472fe671679cd669a89159258697618
Paul-16098 <pl816098@gmail.com>
2024-03-15 21:42:09 +0800

    modified:   "js/userjs/github\347\232\204\351\223\276\346\216\245\345\234\250\346\226\260\346\240\207\347\255\276\351\241\265\346\211\223\345\274\200.js"

--- js/userjs/github的链接在新标签页打开.js
+++ js/userjs/github的链接在新标签页打开.js
@@ -1,43 +1,46 @@
 // ==UserScript==
 // @name         github的链接在新标签页打开
 // @namespace    pl816098
-// @version      1.0.1
+// @version      1.0.2
 // @description  让github的链接默认是在新标签页中打开而不是当前页打开
 // @author       pl816098
 // @match        https://github.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @run-at       document-idle
+// @license      MIT
+// @supportURL   https://github.com/Paul-16098/vs_code/issues/
+// @homepageURL  https://github.com/Paul-16098/vs_code/blob/main/js/userjs/README.md
+// @downloadURL  https://github.com/Paul-16098/vs_code/raw/main/js/userjs/github%E7%9A%84%E9%93%BE%E6%8E%A5%E5%9C%A8%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5%E6%89%93%E5%BC%80.js
+// @updateURL    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/github%E7%9A%84%E9%93%BE%E6%8E%A5%E5%9C%A8%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5%E6%89%93%E5%BC%80.js
 // ==/UserScript==
 
 (function () {
   "use strict";
 
-  // 网上百度一直就说href改为_blank就可以了，但是目录页面移入是会在当前页打开。网上找的几个脚步都没有解决这个问题。于是又换了一种思路
-  // 我把默认href禁用，给a标签加一个点击事件。在js中使用window.open的方式打开
-  // 关于不跳转我猜测是用了hash路由或者单页面应用。希望有懂的可以帮忙解答下困惑
   let not_blank = [
     'a[data-testid="raw-button"]',
-    "#repos-sticky-header > div > div > div > div.react-blob-header-edit-and-raw-actions > div:nth-child(1) > a:nth-child(1)",
   ];
+
   setTimeout(() => {
     let links = document.getElementsByTagName("a");
     for (let i = 0; i < links.length; i++) {
-      // 拿到每一个url
+      let found = false;
       for (let ii = 0; ii < not_blank.length; ii++) {
-        let element = document.querySelectorAll(not_blank[ii]);
-        if (element.length !== 1) {
-          console.warn("element.length !== 1");
+        let element = document.querySelector(not_blank[ii]);
+        if (element && element === links[i]) {
+          found = true;
+          console.log("not_blank, element === links[i]", element === links[i]);
+          break;
         }
-        if (element === links) {
-          console.warn("not_blank");
-        } else {
-          let url = links[i].href;
-          links[i].href = "javascript:void(0);";
-          links[i].onclick = function () {
-            window.open(url);
-          };
-          console.log("github的链接在新标签页打开 run done", links[i]);
-        }
+      }
+      if (!found) {
+        let url = links[i].href;
+        links[i].href = "javascript:void(0);";
+        links[i].onclick = function () {
+          window.open(url);
+        };
+        console.log("github的链接在新标签页打开 run done", links[i]);
       }
     }
   }, 2000);
