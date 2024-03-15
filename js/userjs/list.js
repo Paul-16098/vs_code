37e89039ee62290991732064fe3460cac0dab10c
Paul-16098 <pl816098@gmail.com>
2024-03-15 20:57:37 +0800

    modified:   js/userjs/list.js

--- js/userjs/list.js
+++ js/userjs/list.js
@@ -4,7 +4,8 @@
 // @version      1.0.0
 // @author       pl816098
 // @connect      *
-// @scriptUrl    https://scriptcat.org/scripts/code/1587/69shuba%20auto%20%E6%9B%B8%E7%B0%BD.user.js
-// @scriptUrl    https://scriptcat.org/scripts/code/1589/ixdzs8.tw.user.js
-// @scripturl    https://scriptcat.org/scripts/code/1589/ixdzs8.tw.user.js
+// @scriptUrl    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/69shuba%20auto%20%E6%9B%B8%E7%B0%BD.user.js
+// @scriptUrl    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/czbooksnet.user.js
+// @scripturl    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/ixdzs8tw.user.js
+// @scripturl    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/Tools.user.js
 // ==/UserSubscribe==
