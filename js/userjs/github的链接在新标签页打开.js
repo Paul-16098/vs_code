07ed10594b83ae6027b79a5d554ad415c74729ca
Paul-16098 <pl816098@gmail.com>
2024-03-16 09:48:06 +0800

    modified:   "js/userjs/github\347\232\204\351\223\276\346\216\245\345\234\250\346\226\260\346\240\207\347\255\276\351\241\265\346\211\223\345\274\200.js"

--- js/userjs/github的链接在新标签页打开.js
+++ js/userjs/github的链接在新标签页打开.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         github的链接在新标签页打开
 // @namespace    pl816098
-// @version      1.0.9
+// @version      1.0.10
 // @description  让github的链接默认是在新标签页中打开而不是当前页打开
 // @author       pl816098
 // @match        https://github.com/*
@@ -68,7 +68,7 @@ config:
           break;
         }
         let o_url = new URL(url);
-        let patt = /(?:([^:/\\@\s])+\.)*github\.((?!io)|[a-zA-Z]{3})$/i;
+        let patt = /(?:([^:/\\@\s])+\.)*github\.([a-zA-Z]{2,4})$/i;
         if (!patt.test(o_url.hostname)) {
           console.log("patt.test(o_url.hostname)", patt.test(o_url.hostname));
           if (host) {
