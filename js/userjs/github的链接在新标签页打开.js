a35924a6a828c8657ebb13c983fe9a186ba73322
Paul-16098 <pl816098@gmail.com>
2024-03-16 08:30:04 +0800

    modified:   "js/userjs/github\347\232\204\351\223\276\346\216\245\345\234\250\346\226\260\346\240\207\347\255\276\351\241\265\346\211\223\345\274\200.js" 	new file:   "js/userjs/github\347\232\204\351\223\276\346\216\245\345\234\250\346\226\260\346\240\207\347\255\276\351\241\265\346\211\223\345\274\200.yml"

--- js/userjs/github的链接在新标签页打开.js
+++ js/userjs/github的链接在新标签页打开.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         github的链接在新标签页打开
 // @namespace    pl816098
-// @version      1.0.3
+// @version      1.0.4
 // @description  让github的链接默认是在新标签页中打开而不是当前页打开
 // @author       pl816098
 // @match        https://github.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @grant        GM_getValue
 // @run-at       document-idle
 // @license      MIT
 // @supportURL   https://github.com/Paul-16098/vs_code/issues/
@@ -14,11 +14,20 @@
 // @downloadURL  https://github.com/Paul-16098/vs_code/raw/main/js/userjs/github%E7%9A%84%E9%93%BE%E6%8E%A5%E5%9C%A8%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5%E6%89%93%E5%BC%80.js
 // @updateURL    https://github.com/Paul-16098/vs_code/raw/main/js/userjs/github%E7%9A%84%E9%93%BE%E6%8E%A5%E5%9C%A8%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5%E6%89%93%E5%BC%80.js
 // ==/UserScript==
+/* ==UserConfig==
+config:
+  host:
+    title: 是否取消對外部域名的處理
+    description: 是否取消對外部域名的處理
+    type: checkbox
+    default: true
+ ==/UserConfig== */
 
 (function () {
   "use strict";
 
-  let not_blank = ['a[data-testid="raw-button"]'];
+  const host = GM_getValue("config.host", true);
+  const not_blank = ['a[data-testid="raw-button"]'];
 
   setTimeout(() => {
     let links = document.getElementsByTagName("a");
@@ -33,11 +42,18 @@
       }
       if (!found) {
         let url = links[i].href;
+        let o_url = new URL(url);
+        if (o_url.hostname !== "github.com") {
+          if (host) {
+            console.log('o_url.hostname !== "github.com"');
+            break;
+          }
+        }
         links[i].href = "javascript:void(0);";
         links[i].onclick = function () {
           window.open(url);
         };
-        console.log("github的链接在新标签页打开 run done", links[i]);
+        // console.log("github的链接在新标签页打开 run done", links[i]);
       }
     }
   }, 2000);
