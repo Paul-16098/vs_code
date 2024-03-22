c054e4fb4d93d0bffb88b3f9aad7256dba6be4c3
Paul-16098 <pl816098@gmail.com>
2024-03-22 20:02:44 +0800

    modified:   py/auto_py_to_exe.bat

--- py/auto_py_to_exe.bat
+++ py/auto_py_to_exe.bat
@@ -1,9 +1,10 @@
 echo off
 cls
 set "py_path=%1"
+set "script_dir=%~dp0"
 if "%py_path%"=="" (
-    py -m auto_py_to_exe -nc -lang zh_tw -c "C:\Users\p\Documents\vs_code\py\default_cofg.json"
+    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%output" -c "C:\Users\p\Documents\vs_code\py\default_cofg.json"
 ) else (
-    py -m auto_py_to_exe -nc -lang zh_tw -c "C:\Users\p\Documents\vs_code\py\default_cofg.json" "%py_path%"
+    py -m auto_py_to_exe -nc -lang zh_tw -o "%script_dir%output" -c "C:\Users\p\Documents\vs_code\py\default_cofg.json" "%py_path%"
 )
 exit 0
