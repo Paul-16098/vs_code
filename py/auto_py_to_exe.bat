8df65d75aea8a979b5df04c7afbec879b4d1f12e
Paul-16098 <pl816098@gmail.com>
2024-03-22 19:44:22 +0800

    modified:   .gitignore 	modified:   py/auto_py_to_exe.bat 	new file:   py/default_cofg.json

--- py/auto_py_to_exe.bat
+++ py/auto_py_to_exe.bat
@@ -2,8 +2,8 @@ echo off
 cls
 set "py_path=%1"
 if "%py_path%"=="" (
-    py -m auto_py_to_exe -lang zh_tw
+    py -m auto_py_to_exe -nc -lang zh_tw -c "C:\Users\p\Documents\vs_code\py\default_cofg.json"
 ) else (
-    py -m auto_py_to_exe -lang zh_tw "%py_path%"
+    py -m auto_py_to_exe -nc -lang zh_tw -c "C:\Users\p\Documents\vs_code\py\default_cofg.json" "%py_path%"
 )
 exit 0
