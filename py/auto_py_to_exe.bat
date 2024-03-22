0ddd00567f57e25f336d663aa192999a0c3ee4c1
Paul-16098 <pl816098@gmail.com>
2024-03-22 19:29:44 +0800

    modified:   py/auto_py_to_exe.bat

--- py/auto_py_to_exe.bat
+++ py/auto_py_to_exe.bat
@@ -1,4 +1,9 @@
 echo off
 cls
-py -m auto_py_to_exe -lang zh_tw
+set "py_path=%1"
+if "%py_path%"=="" (
+    py -m auto_py_to_exe -lang zh_tw
+) else (
+    py -m auto_py_to_exe -lang zh_tw "%py_path%"
+)
 exit 0
